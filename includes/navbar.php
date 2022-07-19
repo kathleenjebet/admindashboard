4f4808fbf0fab69c92c7b6e2d41251d1429841f3
kathleenjebet <katejebet123@gmail.com>
2022-07-19 13:38:59 +0300

    FIX fix logout button

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,6 +1,8 @@
 <div class="header">
 		<img src="images/zalegooo.png" alt="zalego" height="50" width="50" class="rounded-circle">
-		<span class="pull-right" style="padding-left-8"></span>
-		<a href="logout.php"><i class="fa fa-sign-out fa-2x"></i></a>
+		<span></span>
+		<span class="float-right">
+			<a href="logout.php"><i class="fa fa-sign-out fa-2x"></i></a>
+		</span>
 		<a href="#" class="navbar-trigger">
 	</div>
